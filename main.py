998c3d8c446f225977d59d4bf5cd2163922e4565
ZHdanov Valery <becwal@yandex.ru>
2024-05-19 10:38:34 +0300

    итерация 2 слово по буквам и фон

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ pygame.init()
 
 W, H = 600, 600
 
+sym_w, sym_h = 50,50
+
 sc = pygame.display.set_mode((W,H) )
 pygame.display.set_caption("Wordle")
 
@@ -14,8 +16,10 @@ curr_game=Game()
 
 from sym import Sym
 
-s1=Sym(curr_game.quest, 300,300 )
+s1 = None
+# Sym("ш", W/2,H/4 )
 
+#50 50
 
 while True:
     for event in pygame.event.get():
@@ -26,5 +30,10 @@ while True:
 
 
     sc.fill((0,0,0))
-    sc.blit(s1.text, s1.rect)
+    pos = 0
+    for i in curr_game.quest:
+        s1 = Sym(i, W/3+pos,H/4 )
+        sc.blit(s1.text, s1.rect)
+        pos += sym_w
+    #print(s1.rect.width, s1.rect.height)
     pygame.display.update()
--- sym.py
+++ sym.py
@@ -2,7 +2,7 @@ import pygame
 class Sym(pygame.sprite.Sprite):
     def __init__(self, text,x,y):
         self.textcolor =(130, 207, 29)
-        self.bgcolor = (0,0,0)
+        self.bgcolor = (56,0,0)
         self.font = pygame.font.Font(None, 72)
         self.text = self.font.render(text, True,  self.textcolor, self.bgcolor)
         self.rect = self.text.get_rect(center=(x, y))
